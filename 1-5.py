b6bc1606ed549c0b852ab5fc961389475fba0bf8
Avidal1 <110049516+Avidal1@users.noreply.github.com>
2022-07-26 14:31:50 -0400

    Update 1-5.py

--- 1-5.py
+++ 1-5.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-# password generator no loop
+# password generator no loop - I'll keep this one commented out but you do as you wish
 
 # list_letters = string.ascii_lowercase + string.ascii_uppercase
 # list_numbers = string.digits
@@ -26,34 +26,33 @@ import string
 
 # with a for loop
 
-# ask_letters = int(input("How many letters you want? \n >> "))
-# ask_numbers = int(input("How many numbers you want? \n >> "))
-# ask_symbols = int(input("How many symbols you want? \n >> "))
-#
-# letters = string.ascii_lowercase + string.ascii_uppercase
-# numbers = string.digits
-# symbols = string.punctuation
-#
-# password = []
-# for char in range(1, ask_letters + 1):
-#     password += random.choice(letters)
-#
-#
-# for char in range(1, ask_numbers + 1):
-#     password += random.choice(numbers)
-#
-#
-# for char in range(1, ask_symbols + 1):
-#     password += random.choice(symbols)
-#
-# print(password)
-# random.shuffle(password)
-# # print(*password, sep="")
-# # or below
-# print(password)
-#
-# password_string = ''
-# for char in password:
-#     password_string += char
-#
-# print(password_string)
+ask_letters = int(input("How many letters you want? \n >> "))
+ask_numbers = int(input("How many numbers you want? \n >> "))
+ask_symbols = int(input("How many symbols you want? \n >> "))
+
+letters = string.ascii_lowercase + string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation
+
+password = []
+for char in range(1, ask_letters + 1):
+    password += random.choice(letters)
+
+
+for char in range(1, ask_numbers + 1):
+    password += random.choice(numbers)
+
+
+for char in range(1, ask_symbols + 1):
+    password += random.choice(symbols)
+
+print(password)
+random.shuffle(password)
+
+print(password)
+
+password_string = ''
+for char in password:
+    password_string += char
+
+print(password_string)
